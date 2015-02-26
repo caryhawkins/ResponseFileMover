b22a35a17052345abb05dfaad456ab101dd341c6
Cary Hawkins <caryhawkins@origamiowl.com>
2015-02-25 20:46:51 -0700

    Change File Path

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ResponseFileMover
 {
-    class Program
+  
+    public class SimpleFileMove
     {
-        static void Main(string[] args)
+        static void main()
         {
+            string sourceFile = @"C:\IF_TEST\Z_orderD\OOWL_PH_ORDER_DOWN.RESP";
+            string destinationfile = @"C:\IF_TEST\Z_orderD\bak\OOWL_PH_ORDER_DOWN"+ DateTime.Now.ToString("dd_MM_YYYY_hhmm") + ".RESP";
+            string source2 = @"C:\IF_TEST\Z_orderD\OOWL_PH_ORDER_DOWN";
+            string destination2 = @"C:\IF_TEST\Z_orderD\bak\OOWL_PH_ORDER_DOWN"+ DateTime.Now.ToString("dd_MM_YYYY_hhmm") + ".COMPL";
+
+            //Check if Response file exists
+           if (File.Exists(sourceFile))
+            {
+            //Move Response file to a new location with date in name
+            System.IO.File.Move(sourceFile, destinationfile);
+           }
+           //Check if Compl file exists
+           if (File.Exists(source2))
+            {
+               //Move Compl file to new location with date in name
+                System.IO.File.Move(source2, destination2);
+            }
+
         }
     }
 }
